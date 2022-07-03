f4c366498d6af9ceec5f2103b4c23261275b3d5f
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-07-03 22:29:08 +0900

    Initial commit

---
+++ src/main/java/com/example/basicsecurity/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.example.basicsecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
